ab745d1c7c2fd506bd7631e4721ece2a25ad025f
Rudolf Cardol <r_cardol@hotmail.com>
2024-10-23 21:43:19 +0100

    helper for text highlight

---
+++ src/utils/helpers.tsx
@@ -0,0 +1,20 @@
+// Look inside of the text string for the coloredText prop text in that string
+// and color that part of the text
+export const highlightText = (
+  text: string,
+  searchText?: string,
+  color?: string
+) => {
+  if (!searchText) return [text];
+  const parts = text.split(new RegExp(`(${searchText})`, "gi"));
+
+  return parts.map((part, index) =>
+    part.toLowerCase() === searchText.toLowerCase() ? (
+      <span key={index} style={{ color }}>
+        {part}
+      </span>
+    ) : (
+      part
+    )
+  );
+};
