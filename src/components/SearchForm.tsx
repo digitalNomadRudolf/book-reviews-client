4bea4c7bde4ea1ca2af3aa504b5d2ad6e6486bd4
Rudolf Cardol <r_cardol@hotmail.com>
2024-10-23 21:52:00 +0100

    SearchForm

---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { FormEventHandler, useEffect, useState } from "react";
+
+interface SearchFormProps {
+  initialQuery: string;
+  setInitialQuery: (query: string) => void;
+  handleSubmit?: (e: React.FormEvent) => void;
+}
+
+const SearchForm = ({ initialQuery, setInitialQuery }: SearchFormProps) => {
+  const router = useRouter();
+  const [localQuery, setLocalQuery] = useState(initialQuery);
+
+  useEffect(() => {
+    if (initialQuery !== localQuery) {
+      setLocalQuery(initialQuery);
+    }
+  }, [initialQuery]);
+
+  // handleSearch function that preventsDefault & doesnt do anything if query is empty string
+  const handleSearch: FormEventHandler = (e) => {
+    e.preventDefault();
+
+    if (localQuery) {
+      // Redirect to the search results page with the query as a URL parameter
+      router.push(`/search?query=${encodeURIComponent(localQuery)}`);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSearch} className="flex items-center">
+      <input
+        type="text"
+        placeholder="Title, author, name, publisher..."
+        className="input border-black input-primary w-full h-11 max-w-md mr-2"
+        value={localQuery}
+        onChange={(e) => setLocalQuery(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="btn w-36 h-11 font-normal text-xl font-raleway text-white bg-blue-cream hover:text-white hover:bg-black rounded-xl ml-4"
+      >
+        search
+      </button>
+    </form>
+  );
+};
+
+export default SearchForm;
